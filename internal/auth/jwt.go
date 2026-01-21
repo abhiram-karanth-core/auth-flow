1f3659e093f36bdf9d348d53d293109abc73b18a
Abhiram-Dev2004 <abhiramkaranth22@gmail.com>
2026-01-21 23:14:21 +0530

    add subject in claims

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -6,27 +6,29 @@ import (
 
 	"github.com/golang-jwt/jwt/v5"
 )
-
 type Claims struct {
-	Email string `json:"email"`
-	jwt.RegisteredClaims
+    Email    string `json:"email"`
+    Provider string `json:"provider"`
+    jwt.RegisteredClaims
 }
 
-func GenerateJWT(email string) (string, error) {
-	secret := os.Getenv("JWT_SECRET")
-	if secret == "" {
-		secret = "dev-secret-change-me"
-	}
+func GenerateJWT(email, providerUserID string) (string, error) {
+    secret := os.Getenv("JWT_SECRET")
+    if secret == "" {
+        secret = "dev-secret-change-me"
+    }
 
-	claims := Claims{
-		Email: email,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			Issuer:    "authflow-go",
-		},
-	}
+    claims := Claims{
+        Email:    email,
+        Provider: "google",
+        RegisteredClaims: jwt.RegisteredClaims{
+            Subject:   providerUserID, // ✅ REQUIRED
+            ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+            IssuedAt:  jwt.NewNumericDate(time.Now()),
+            Issuer:    "authflow-go",
+        },
+    }
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(secret))
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString([]byte(secret))
 }
